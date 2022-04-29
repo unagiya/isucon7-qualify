6836e3b4cd2c2764807c030625198b41759d9704
Kazuya Saso <kazsan0215@gmail.com>
2022-04-29 16:43:51 +0900

    プロフィール更新時の画像、BLOB保存→ボリューム保存

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -680,10 +680,12 @@ func postProfile(c echo.Context) error {
 	}
 
 	if avatarName != "" && len(avatarData) > 0 {
-		_, err := db.Exec("INSERT INTO image (name, data) VALUES (?, ?)", avatarName, avatarData)
+		file, err := os.Create("/data/images/" + avatarName)
 		if err != nil {
-			return err
+			panic(err)
 		}
+		defer file.Close()
+
 		_, err = db.Exec("UPDATE user SET avatar_icon = ? WHERE id = ?", avatarName, self.ID)
 		if err != nil {
 			return err
